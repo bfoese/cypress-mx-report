e8d0392b1f78ebb250dcfdc2e27bf32135017eb6
Britt Foese <britt.foese@gmail.com>
2021-04-01 17:45:36 +0200

    fix(media urls): generated urls without hardcoded base path

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,8 +1,16 @@
+const path = require("path");
+
+/**
+ * This code will run in the Cypress plugin context. This context is executed in
+ * a NodeJS environment. Therefore Node specific modules like `path` can be
+ * used.
+ */
 export class CypressMxReportPlugin {
+
   /**
    * This function will register the plugin. The plugin will generate a unique
    * ID for each test run and make sure that screenshots and videos are stored
-   * in a subdirectory of the originial screenshotFolder/videosFolder. The
+   * in a subdirectory of the original screenshotFolder/videosFolder. The
    * subdirectory will be named with the unique ID of the test run.
    *
    * This way, you can have multiple cypress runs with different
@@ -12,8 +20,6 @@ export class CypressMxReportPlugin {
    * Must be called from cypress/plugins/index.ts
    *
    * @param config - Cypress config object
-   *
-   *
    * @returns A config object with modified screenshotsFolder and videosFolder
    * path. In order to have this config applied, you need to return in from your
    * module.exports function in cypress/plugins/index.js.
@@ -22,24 +28,54 @@ export class CypressMxReportPlugin {
    * https://docs.cypress.io/guides/tooling/plugins-guide#Using-a-plugin
    */
   public static register(config: any): any {
-    const path = require("path");
-
+    // if user provided a test run ID prefer this, otherwise generate one
     const mxTestRunId =
       config?.env?.mxTestRunId ??
       `${new Date().getTime()}-${Math.floor(Math.random() * 1000)}`;
 
     if (config?.screenshotsFolder) {
+      const screenshotsFolderName = this.lastPathSegment(config.screenshotsFolder);
       config.screenshotsFolder = path.join(
         config.screenshotsFolder,
         mxTestRunId
       );
+      // Provide the new screenshotsFolder sub-path as env variable. This will
+      // be used in the support file to generate URLs for the screenshots. Use
+      // slash concatenation instad of path.join() - slash is supported on all
+      // platforms and path module is not available in support file (browser
+      // environment) so it will enhance the path with slashes as well
+      config.env = Object.assign(config.env ?? {}, { mxScreenshotsFolder: `${screenshotsFolderName}/${mxTestRunId}` });
     }
 
     if (config?.videosFolder) {
+      const videosFolderName = this.lastPathSegment(config.videosFolder);
       config.videosFolder = path.join(config.videosFolder, mxTestRunId);
+
+      // Provide the new videosFolder sub-path as env variable. This will be used in the support file to generate URLs for the videos
+      config.env = Object.assign(config.env ?? {}, { mxVideosFolder: `${videosFolderName}/${mxTestRunId}` });
     }
-    // mxTestRunId will be used in support file to create the correct screenshot/video URL
-    config.env = Object.assign(config.env ?? {}, { mxTestRunId: mxTestRunId });
     return config;
+  }
+
+
+  /**
+   * Extract the last segment from a path in a platform agnostic manner. I do
+   * not use 'path' module here, because it gived no control over trailing
+   * slash/backslash.
+   *
+   * @param path - Unix or Windows path, with or without trailing
+   * slash/backslash, e.g. `foo/bar/baz`, `foo/bar/baz/`, `foo\\bar\\baz`,
+   * `foo\\bar\\baz\\`
+   * @returns Last path segment from the given path. If the path ends with a
+   * trailing slash/backslash, the segment before will be returned. These
+   * `foo/bar/baz`, `foo/bar/baz/`, `foo\\bar\\baz`, `foo\\bar\\baz\\` path
+   * definitions will all return the same result, which is `baz`.
+   */
+   private static lastPathSegment(path: string | undefined): string | undefined {
+    if (path) {
+      const normalizedPath = path.replace(/\\/g, '/').replace(/\/$/, '');
+      return normalizedPath.substring(normalizedPath.lastIndexOf('/') + 1);
+    }
+    return undefined;
   }
 }
--- src/support.ts
+++ src/support.ts
@@ -1,4 +1,34 @@
+const addContext = require('mochawesome/addContext');
+
+export type CypressMxReportOptions = { screenshots: boolean, videos: boolean};
+
+/**
+ * These scripts are meant to be called from `cypress/support/index.ts` and
+ * therefore will run in the browser and not in a NodeJS environment. Make sure
+ * to only rely on APIs which are supported in the browser.
+ */
 export class CypressMxReport {
+
+  /**
+   * This function will append a screenshot link to the Mochawesome spec context
+   * object. This will enhance your Mochawesome Cypress report with screenshots
+   * of failed tests.
+   *
+   * Must be called from support/index.ts
+   * @param Cypress
+   * @param opts - If options are not provided, all media files will be attached
+   * to the report. Otherwise you can use the options object to customize which
+   * media files should be attached.
+   */
+  public static includeMedia(Cypress, opts?: CypressMxReportOptions): void {
+    if (!opts || opts.screenshots) {
+      this.includeScreenshots(Cypress);
+    }
+
+    if (!opts || opts.videos) {
+      this.includeVideos(Cypress);
+    }
+  }
   /**
    * This function will append a screenshot link to the Mochawesome spec context
    * object. This will enhance your Mochawesome Cypress report with screenshots
@@ -7,14 +37,12 @@ export class CypressMxReport {
    * Must be called from support/index.ts
    * @param Cypress
    */
-  public static includeScreenshots(Cypress) {
-    Cypress.on("test:after:run", (test: any, runnable: any) => {
-      const addContext = require("mochawesome/addContext");
+  public static includeScreenshots(Cypress): void {
+    Cypress.on('test:after:run', (test: any, runnable: any) => {
+      const mxScreenshotsFolder = Cypress.env('mxScreenshotsFolder');
 
-      const mxTestRunId = Cypress.env("mxTestRunId");
-
-      if (mxTestRunId) {
-        if (test.state === "failed") {
+      if (mxScreenshotsFolder) {
+        if (test.state === 'failed') {
           // Cypress generates the name of the screenshot based on the test suite structure. We need to re-create that.
           let item: any = runnable;
           const nameParts = [runnable.title];
@@ -24,8 +52,8 @@ export class CypressMxReport {
             nameParts.unshift(item.parent.title);
             item = item.parent;
           }
-          const fullTestName = nameParts.filter(Boolean).join(" -- ");
-          const imageUrl = `screenshots/${mxTestRunId}/${Cypress.spec.name}/${fullTestName} (failed).png`;
+          const fullTestName = nameParts.filter(Boolean).join(' -- ');
+          const imageUrl = `${mxScreenshotsFolder}/${Cypress.spec.name}/${fullTestName} (failed).png`;
           addContext({ test }, imageUrl);
         }
       }
@@ -40,18 +68,16 @@ export class CypressMxReport {
    * @param Cypress
    */
   public static includeVideos(Cypress): void {
-    Cypress.on("test:after:run", (test: any, runnable: any) => {
-      const addContext = require("mochawesome/addContext");
-
-      const mxTestRunId = Cypress.env("mxTestRunId");
-
-      if (mxTestRunId) {
-        const videoUrl = `videos/${mxTestRunId}/${Cypress.spec.name.replace(
-          "/.ts.*",
-          ".ts"
+    Cypress.on('test:after:run', (test: any, runnable: any) => {
+      const mxVideosFolder = Cypress.env('mxVideosFolder');
+      if (mxVideosFolder) {
+        const videoUrl = `${mxVideosFolder}/${Cypress.spec.name.replace(
+          '/.ts.*',
+          '.ts'
         )}.mp4`;
         addContext({ test }, videoUrl);
       }
     });
   }
+
 }
